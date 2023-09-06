16fe84fce546873a6e4e61a19c360e797d7c3f65
shahadathossain4536 <shahadathossain4536@gmail.com>
2023-09-06 19:57:01 +0600

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between pt-24 pb-24 bg-white ">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-white ">
       <PopupForm/>
     </main>
   )
